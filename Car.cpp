d2ce682e3c200f9ca620fb81b78ff30cdb0cd3e6
rpdunn77 <rp.dunn@yahoo.ca>
2016-02-15 13:34:07 -0800

    Code cleanup 1.

--- Car.cpp
+++ Car.cpp
@@ -15,13 +15,8 @@ class ImageLoader;
 void Car::left ()
 {
    if(!stopleft){
-   	if(Game::getInstance().getArrayPos() == 3 || Game::getInstance().getArrayPos() == 1){
-      	m_arraypos = Game::getInstance().getArrayPos() +1;
-   	}
-   	else{
-      	m_arraypos = Game::getInstance().getArrayPos() - 1;
-   	}
-      
+      m_arraypos = Game::getInstance().getArrayPos() - 1;
+    
    	if(m_x-5 <= 0)
    	{
       	if(Game::getInstance().getArrayPos() != 3 && Game::getInstance().getArrayPos() != 1){
@@ -30,7 +25,7 @@ void Car::left ()
 	      	m_x = 968 ;
       	}
    	}else{
-	   	m_x -= 4*m_speed;//50
+	   	m_x -= 4*m_speed;
    	}
    }
    m_direction = 3;
@@ -39,12 +34,7 @@ void Car::left ()
 void Car::right ()
 {
    if(!stopright){
-   	if(Game::getInstance().getArrayPos() == 1 || Game::getInstance().getArrayPos() == 3){
-      	m_arraypos = Game::getInstance().getArrayPos() +1;
-   	}
-   	else{
-      	m_arraypos = Game::getInstance().getArrayPos() - 1;
-   	}
+      m_arraypos = Game::getInstance().getArrayPos() - 1;
 
    	if(m_x+55 >= 1023)
    	{
@@ -63,12 +53,7 @@ void Car::right ()
 void Car::up ()
 {
    if(!stopup){
-   	if(Game::getInstance().getArrayPos() == 1 || Game::getInstance().getArrayPos() == 2){
-      	m_arraypos = Game::getInstance().getArrayPos() +2;
-   	}
-   	else{
-      	m_arraypos = Game::getInstance().getArrayPos() - 2;
-   	}
+      m_arraypos = Game::getInstance().getArrayPos() - 2;
 
   		if(m_y+60 >= 731)
   		{
@@ -87,12 +72,7 @@ void Car::up ()
 void Car::down ()
 {
    if(!stopdown){
-   	if(Game::getInstance().getArrayPos() == 1 || Game::getInstance().getArrayPos() == 2){
-      	m_arraypos = Game::getInstance().getArrayPos() +2;
-   	}
-   	else{
-      	m_arraypos = Game::getInstance().getArrayPos() - 2;
-   	}
+      m_arraypos = Game::getInstance().getArrayPos() - 2;
 
   		if(m_y-5 <= 0)
   		{
@@ -105,8 +85,8 @@ void Car::down ()
 			m_y -= 4 * m_speed;
   		}
    }
-   m_direction = 2;
-  m_carTexture= ImageLoader::LoadTexture( "./imgs/Down.bmp" );
+	m_direction = 2;
+  	m_carTexture= ImageLoader::LoadTexture( "./imgs/Down.bmp" );
 }
 void Car::attack ()
 {
@@ -175,187 +155,53 @@ void Car::collisions(int xpos, int ypos, int width, int height, int cond,int qua
 		float circledistancey = abs(ypos - (m_y + (m_size/2)));
 		float corner = pow((circledistancex - m_size/2),2) +
 							pow((circledistancey - m_size/2),2);
-		//float corner = pow(circledistancex,2) + pow(circledistancey,2);
-		//corner = sqrt(corner);
 		if(circledistancex > (m_size/2 + width)){	
 			//m_speed = 1;
-			stopup = false;
-      	stopdown = false;
-      	stopleft = false;
-      	stopright = false;
 		}else if(circledistancey > (m_size/2 + width)){	
 			//m_speed = 1;
-			stopup = false;
-      	stopdown = false;
-      	stopleft = false;
-      	stopright = false;
 		}else if(circledistancex <= (m_size/2)){
       	//m_speed = 0.5;
       	if(c_up){
-      				//stopup = true;
-      				//stopdown = false;
-      				//stopleft = false;
-      				//stopright = false;
-      				m_y -=4;
+      		m_y -=4;
       	}
       	if(c_right){
-      				//stopright = true;
-      				//stopleft=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x -=4;
+      		m_x -=4;
       	}
       	if(c_down){
-      				//stopdown = true;
-      				//stopup=false;
-      				//stopright=false;
-      				//stopleft=false;
-      				m_y +=4;
+      		m_y +=4;
       	}
       	if(c_left){
-      				//stopleft = true;
-      				//stopright=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x +=4;
+      		m_x +=4;
       	}
 		}else if(circledistancey <= (m_size/2)){
 			//m_speed = 0.5;
 			if(c_up){
-      				//stopup = true;
-      				//stopdown = false;
-      				//stopleft = false;
-      				//stopright = false;
-      				m_y -=4;
+      		m_y -=4;
       	}
       	if(c_right){
-      				//stopright = true;
-      				//stopleft=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x -=4;
+      		m_x -=4;
       	}
       	if(c_down){
-      				//stopdown = true;
-      				//stopup=false;
-      				//stopright=false;
-      				//stopleft=false;
-      				m_y +=4;
+      		m_y +=4;
       	}
       	if(c_left){
-      				//stopleft = true;
-      				//stopright=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x +=4;
-      		}
-		//}else if(corner <= width + sqrt(pow(m_size/2,2)+pow(m_size/2,2))){
+      		m_x +=4;
+      	}
 		}else if(corner <=pow(width,2)){
 			//m_speed = 0.5;
       	if(c_up){
-      				//stopup = true;
-      				//stopdown = false;
-      				//stopleft = false;
-      				//stopright = false;
-      				m_y -=4;
+      		m_y -=4;
       	}
       	if(c_right){
-      				//stopright = true;
-      				//stopleft=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x -=4;
+      		m_x -=4;
       	}
       	if(c_down){
-      				//stopdown = true;
-      				//stopup=false;
-      				//stopright=false;
-      				//stopleft=false;
-      				m_y +=4;
+      		m_y +=4;
       	}
       	if(c_left){
-      				//stopleft = true;
-      				//stopright=false;
-      				//stopup=false;
-      				//stopdown=false;
-      				m_x +=4;
+      		m_x +=4;
       	}
 		}
-     
-     
-     /*
-     if(m_direction == 0){
-      				stopup = true;
-      				stopdown = false;
-      				stopleft = false;
-      				stopright = false;
-      				m_y -=5;
-      		}else if(m_direction == 1){
-      				stopright = true;
-      				stopleft=false;
-      				stopup=false;
-      				stopdown=false;
-      				m_x -=5;
-      		}else if(m_direction == 2){
-      				stopdown = true;
-      				stopup=false;
-      				stopright=false;
-      				stopleft=false;
-      				m_y +=5;
-      		}else if(m_direction == 3){
-      				stopleft = true;
-      				stopright=false;
-      				stopup=false;
-      				stopdown=false;
-      				m_x +=5;
-      		}
-     */
-     
-     
-     
-     
-     
-     	/*for(int i = 0; i<200;i++){
-      	double angle = 2*i*M_PI/200;
-      	float newX = xpos + width*cos(angle);
-      	float newY = ypos + width*sin(angle);
-      	
-      	
-      	if(newX+5 > m_x && newX < m_x+m_size && newY > m_y && newY < m_y+m_size){
-      		stopleft = true;
-      	}else if(newX-5 < m_x+m_size && newX > m_x && newY > m_y && newY < m_y+m_size){
-      		stopright = true;
-      	}else if(newY-5 < m_y+m_size && newY > m_y && newX > m_x && newX < m_x+m_size){
-      		stopup = true;
-      	}else if(newY+5 > m_y && newY < m_y+m_size && newX> m_x && newX < m_x+m_size){
-      		stopdown = true;
-      	}else{
-         	stopright = false;
-         	stopleft = false;
-         	stopup = false;
-         	stopdown = false;
-      	}
-      	if(stopright == true || stopleft == true||stopup==true||stopdown==true)
-      		break;
-      	/*
-      	
-      	/*if(newX+5 > m_x && newX-5 < m_x+m_size && newY+5 > m_y && newY-5 < m_y+m_size){
-      		if(m_direction == 0){
-      				stopup = true;
-      		}else if(m_direction == 1){
-      				stopright = true;
-      		}else if(m_direction == 2){
-      				stopdown = true;
-      		}else if(m_direction == 3){
-      				stopleft = true;
-      		}else{
-      			stopup = false;
-					stopright = false;
-					stopdown = false;	
-					stopleft = false;
-      		}
-      	}
-      }*/
 	}
 }
 
